608d8fd9fb4c6c455d3d3aefc4305d5f8cd21ec9
k4hun <thameth@gmail.com>
2015-01-20 22:10:41 +0100

    category model

--- db/migrate/20150120210644_create_categories.rb
+++ db/migrate/20150120210644_create_categories.rb
@@ -1,7 +1,9 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-
+      t.string "name"
+      t.integer "position"
+      t.boolean "visible", :default => true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120205018) do
+ActiveRecord::Schema.define(version: 20150120210644) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.integer  "position"
+    t.boolean  "visible",    default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "firstname",       limit: 20
