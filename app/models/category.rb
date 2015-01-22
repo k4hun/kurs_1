b592973eda1d622f0fee6aaaa945ef2f062efd84
k4hun <thameth@gmail.com>
2015-01-22 16:48:10 +0100

    add some scopes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ActiveRecord::Base
   has_many :pages
+
+  scope :visible, lambda { where(:visible => true)}
+  scope :sort, lambda { order("categories.position ASC")}
+  scope :latest, lambda { order ("categories.created_at DESC")}
+
 end
