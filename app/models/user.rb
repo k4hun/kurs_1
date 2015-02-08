916fc7f8be379eec9719629f402e4fd7e26b56e7
k4hun <thameth@gmail.com>
2015-02-08 14:49:42 +0100

    modify user model and add user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2-4}\Z/i
+
+  validates :firstname, :lastname,
+            presence: true
+
+  validates :user, 
+            length: { in: 4..25 },
+            uniqueness: true
+
+  validates :email,
+            presence: true,
+            length: { maximum: 100 },
+            format: EMAIL_REGEX,
+            confirmation: true
+
+  scope :sort, lambda{order('lastname ASC, firstname ASC')}
+
 end
