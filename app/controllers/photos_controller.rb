f9743516950194c506653007d85a76f1bd3832fb
k4hun <thameth@gmail.com>
2015-02-08 18:17:09 +0100

    fix views for photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,21 +2,22 @@ class PhotosController < ApplicationController
   
   layout 'admin'
   before_action :check_login
+  before_action :find_gallery
 
   def index
-    @photos = Photo.sort
+    @photos = @gallery.photos.sort
   end
 
   def new
     @gal = Gallery.order('position ASC')
-    @photo = Photo.new(:name => "Enter name")
+    @photo = Photo.new(:name => "Enter name", :gallery_id => @gallery.id)
     @counter = Photo.count + 1
   end
 
   def create
     @photos = Photo.new(photo_params)
     if @photos.save
-      redirect_to( :action => 'index' )
+      redirect_to( :action => 'index', :gallery_id => @gallery.id )
       flash[:notice] = "Photo added!"
     else
       new
@@ -36,7 +37,7 @@ class PhotosController < ApplicationController
 
   def update
     if current_photo.update_attributes(photo_params)
-      redirect_to(:action => 'show', :id => current_photo.id)
+      redirect_to(:action => 'show', :id => current_photo.id, :gallery_id => @gallery.id)
       flash[:notice] = "Photo updated!"
     else 
       edit     
@@ -46,15 +47,23 @@ class PhotosController < ApplicationController
 
   def destroy
     current_photo.destroy
-    redirect_to(:action => 'index')
+    redirect_to(:action => 'index', :gallery_id => @gallery.id)
     flash[:notice] = "Photo destroyed!"
-  end
-
-  def photo_params
-    params.require(:photo).permit(:name, :position, :visible, :photo, :description, :gallery_id)
   end
 
   def current_photo
     Photo.find(params[:id])
   end
+
+private
+  def photo_params
+    params.require(:photo).permit(:name, :position, :visible, :photo, :description, :gallery_id)
+  end
+
+  def find_gallery
+    if params[:gallery_id] 
+      @gallery = Gallery.find(params[:gallery_id])
+    end    
+  end
+
 end
